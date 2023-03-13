dc049358ff52b31eec8b966235d37a1ee5ca46f2
fjolivo <fjolivo@gmail.com>
2023-03-12 21:57:34 -0400

    Update script.js
    loader element

---
+++ client/script.js
@@ -0,0 +1,20 @@
+import bot from './assets/bog.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('from');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval (() => {
+    element.textContent += '.';
+
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300)
+}
+
