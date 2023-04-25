d18c33070ab14a0eb77cabea665afce3cc13f5ad
fjolivo <fjolivo@gmail.com>
2023-04-25 14:09:30 -0400

    Server and Script updates

--- client/script.js
+++ client/script.js
@@ -23,7 +23,7 @@ function typeText(element, text) {
 
   let interval = setInterval (() => {
     if(index < text.length) {
-      element.innerHTML += text.chartAt(index);
+      element.innerHTML += text.charAt(index);
       index++;
     } else {
       clearInterval(interval);
@@ -44,7 +44,7 @@ function chatStripe  (isAi, value, uniqueId) {
     `
       <div class="wrapper ${isAi && 'ai'}">
         <div class="chat">
-          <div className="profile">
+          <div class="profile">
             <img
               src="${isAi ? bot : user}"
               alt="${isAi ? 'bot' : 'user'}"
@@ -76,6 +76,34 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
 
   loader(messageDiv);
+
+  // fetch data from server -> bot's response
+
+  const response = await fetch ('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;'
+    },
+    body:JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+  clearInterval(loadInterval);
+  messageDiv.innderHTML = '';
+
+  if(response.ok) {
+    const data = await response.json();
+    const pasedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong";
+
+    alert(err);
+  }
 
 }
 
--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
+console.log(process.env.OPENAI_API_KEY)
+
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -22,6 +24,26 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/', async (req, res) => {
-    
-}
-)
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p:1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error })
+    }
+})
+
+app.listen(5000, () => console.log('Sever is running on port http://localhost:5000'));
